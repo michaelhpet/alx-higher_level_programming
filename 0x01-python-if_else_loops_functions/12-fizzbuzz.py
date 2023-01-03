7f699f82b88a7633ce52bacdc478aa57c5b2c882
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-03 13:53:07 +0100

    fix: fizzbuzz trailing whitespace

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,10 @@
 def fizzbuzz():
     for number in range(1, 101):
         if number % 15 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif number % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         elif number % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print(f"{number:d}", end="")
-
-        if number < 100:
-            print(" ", end="")
+            print(f"{number:d}", end=" ")
