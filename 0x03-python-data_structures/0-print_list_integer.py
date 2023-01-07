ede91ee4cddbc66b3994813f8294f43b99e653e9
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-07 08:55:02 +0100

    refactor: no report

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{0:d}".format(integer))
+    my_list_len = len(my_list)
+    for i in range(my_list_len):
+        print("{0:d}".format(my_list[i]))
