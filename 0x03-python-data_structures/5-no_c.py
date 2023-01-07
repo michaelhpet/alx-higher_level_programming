94a1a61192682a3f774bf20c47ffdc542bdc1fcd
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-07 09:23:11 +0100

    fix: errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not isinstance(my_string, string):
+    if not isinstance(my_string, str):
         return None
-        
+
     new_string = ""
     for character in my_string:
         if character in "cC":
