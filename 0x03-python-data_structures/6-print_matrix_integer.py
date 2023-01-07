adfef9658e9e4092b8edf536601d72b78aae32ac
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-07 09:29:09 +0100

    feat: proper formatting of matrix printing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for index, element in enumerate(row):
-            end = "\n" if index == len(row) - 1 else " "
+            end = "" if index == len(row) - 1 else " "
             print("{:d}".format(element), end=end)
+        print()
