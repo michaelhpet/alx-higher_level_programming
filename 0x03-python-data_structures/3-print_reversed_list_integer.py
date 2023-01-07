a30eadc75d9a88bc487829edd58a34d4b8527ee0
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-07 09:16:59 +0100

    feat: checks if variable is a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list_len = len(my_list)
+    if not isinstance(my_list, list):
+        return None
 
+    my_list_len = len(my_list)
     for i in range(my_list_len - 1, -1, -1):
         print("{:d}".format(my_list[i]))
