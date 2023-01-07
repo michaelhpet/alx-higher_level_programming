3ecca5cc2dc146c3c33060b3a6e3c90c5363d430
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-07 09:19:22 +0100

    feat: type checks for arguments

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if not isinstance(my_list):
+        return None
+        
     my_list_len = len(my_list)
     if idx < 0 or idx > my_list_len - 1:
         return my_list[:]
