d8529f6659c0f8d4c9a59e5b7dc77643cb1f1b29
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-11 13:59:16 +0100

    feat: roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    token = roman_string.upper()
+    numerals = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500
+    }
+    end = len(token) - 1
+    result = 0
+
+    for idx, char in enumerate(token):
+        if idx < end and numerals[char] < numerals[token[idx + 1]]:
+            result -= numerals[char]
+        else:
+            result += numerals[char]
+
+    return result
