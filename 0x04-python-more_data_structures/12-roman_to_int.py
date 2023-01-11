9a907d666b67242ebde30aa58fa049ed18eaec9f
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-11 14:07:10 +0100

    fix: type checking for roman_string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    if roman_string is None:
+        return 0
+        
     token = roman_string.upper()
     numerals = {
         "I": 1, "V": 5, "X": 10, "L": 50,
