45748443f3bec306a9a013a87f25d89d0e18574b
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-23 21:18:12 +0100

    feat: magic calculation - bytecode to py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,6 +3,13 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
+            if i > a:
+                raise Exception("Too far")
             result = a ** b
+            result = result / i
+            result = i + result
         except Exception:
-            raise Exception("Too far")
+            result = a + b
+            break
+
+    return result
