42e9b004b6edaff86585824bfd4dc67ca5d0fdec
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-23 20:15:13 +0100

    refactor: removed break statement

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,7 +14,6 @@ def list_division(my_list_1, my_list_2, list_len):
         except IndexError:
             print("out of range")
             new_list.append(0)
-            break
         finally:
             i += 1
     return new_list
