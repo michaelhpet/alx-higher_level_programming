b7cd4e9eb3a20f49e2362b16bb232c82fc80aa9a
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-23 20:12:54 +0100

    feat: raise exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_len):
+    i = 0
+    new_list = []
+    while i < list_len:
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            new_list.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+            break
+        finally:
+            i += 1
+    return new_list
