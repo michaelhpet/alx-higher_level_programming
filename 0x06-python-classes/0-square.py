1e6eb85041f607950538f30bfa88a328e8818c53
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-24 10:55:38 +0100

    feat: wrote empty class Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty class
+
+This module demonstrates the creation of empty classes
+in Python
+"""
+class Square:
+    """An empty class Square that defines a square
+    """
+    pass
