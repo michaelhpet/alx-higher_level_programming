2e126e6ceaa8d33b9662f97b89b3a3fe545d4071
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-24 22:44:42 +0100

    feat: wrote magic class for a Circle

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import math
+"""Decoding python ByteCodes that use classes"""
+
+
+class MagicClass:
+    """Blueprint for Magic"""
+    def __init__(self, radius=0):
+        """Initializes a MagicClass instance"""
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Computes the area of a MagicClass instance"""
+        return (getattr(self, self.__radius) ** 2) * (getattr(math, pi))
+
+    def circumference(self):
+        """Computes the circumference of a MagicClass instance"""
+        return (2 * getattr(math, pi) * getattr(self, self.__radius))
