73724a6c152494b9cba50375e23a5086cbb87e64
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-31 16:44:42 +0100

    refactor: using special name __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,8 +7,4 @@ class LockedClass:
     instance attributes, except if the instance attribute
     is called ``first_name``
     """
-    def __setattr__(self, name, value):
-        """Controls dynamic attribute assignments"""
-        if name != "first_name":
-            raise AttributeError('object has no attribute {:s}'.format(value))
-        super().__setattr__(self, name, value)
+    __slots__ = ['first_name']
