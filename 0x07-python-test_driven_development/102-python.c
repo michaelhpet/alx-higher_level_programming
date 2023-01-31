8ec566e8ea9c019403f242459fed8e5d24d3ddfb
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-01 00:03:31 +0100

    refactor: fixed betty styles

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -7,6 +7,8 @@
  */
 void print_python_string(PyObject *p)
 {
+	long int len;
+
 	fflush(stdout);
 	printf("[.] string object info\n");
 
@@ -21,6 +23,7 @@ void print_python_string(PyObject *p)
 	else
 		printf("  type: compact unicode object\n");
 
-	printf("  length: %ld\n", ((PyASCIIObject *)(p))->length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &(((PyASCIIObject *)(p))->length)));
+	len = ((PyASCIIObject *)(p))->length;
+	printf("  length: %ld\n", len);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
 }
