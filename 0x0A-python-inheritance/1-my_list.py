1d1a2fc2db4976b6e71bbc2b5ff995d8d515b345
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 12:09:33 +0100

    fix: added super init method to MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 class MyList(list):
     """Subclass of list"""
 
+    def __init__(self):
+        """Initialized a MyList instance"""
+        super().__init__()
+
     def print_sorted(self):
         """Prints the list in ascending order"""
         print(sorted(self))
