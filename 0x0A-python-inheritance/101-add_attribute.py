0572e79ab7b92d13071f8c2fc9d4156f176df231
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 12:00:14 +0100

    feat: wrote add_attribute()

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/python3
+"""Adding attributes to objects if possible"""
+
+
+def add_attribute(an_object, key, value):
+    """adds a new attribute to an object if possible
+    Args:
+        an_object (object): object to add attribute to
+        key (string): attribute key
+        value (object): attribute value
+    Raises:
+        TypeError: can't add new attribute (if attribute cannot be added)
+    """
+    if not hasattr(an_object, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(an_object, key, value)
