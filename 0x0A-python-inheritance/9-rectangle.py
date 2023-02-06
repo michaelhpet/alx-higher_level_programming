0349b980783a91f77368bc94a7f18fddaad2b693
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 11:29:25 +0100

    feat: implemented area() method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Working with Python inheritance"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a Rectangle object"""
+
+    def __init__(self, width, height):
+        """Initializes a Rectangle object"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the Rectangle object
+        Returns:
+            int: area of instance
+        """
+        return self.__width * self.__height
