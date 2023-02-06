97e24f54823fe10680f6cb06991ce1a77c8737ab
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 11:36:37 +0100

    feat: wrote new Squre subclass

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Working with Python inheritance"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square object"""
+
+    def __init__(self, size):
+        """Initializes a Square object"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the Square instance
+        Returns:
+            int: area of instance
+        """
+        return self.__size ** 2
