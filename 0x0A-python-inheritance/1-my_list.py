b49f792b800d7557952ab78b8a5e06152bfadfe2
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 12:33:26 +0100

    chore: no report

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-Python inheritance
+"""Working with Python inheritance
+This module contains only one class, MyList
 """
 
 
 class MyList(list):
-    """
-    Subclass of list
+    """Subclass of list
     """
 
     def __init__(self):
@@ -14,7 +13,6 @@ class MyList(list):
         super().__init__()
 
     def print_sorted(self):
-        """
-        Prints the list in ascending order
+        """Prints the list in ascending order
         """
         print(sorted(self))
