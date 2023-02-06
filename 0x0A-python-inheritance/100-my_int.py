7bc84f52ccaf61700b53ed8624a8a68f180dd8be
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 11:50:55 +0100

    docs: added docstrings

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,8 +4,11 @@
 
 class MyInt(int):
     """Defines MyInt object"""
+
+    """equal-to operator"""
     def __eq__(self, other):
         return self != other
 
     def __ne__(self, other):
+        """not-equal-to operator"""
         return self == other
