ca46ce39817dc4d57e55dc8337f6d2d7b9f19e93
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-06 11:08:43 +0100

    feat: wrote method integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Working with Python inheritance"""
+
+
+class BaseGeometry:
+    """Defines a base geometry object"""
+    def area(self):
+        """Raises an Exception that area is not implemented
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_value(self, name, value):
+        """Validates the value
+        Raises:
+            TypeError: <name> must be an in integer
+            ValueError: <name> must be greater than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
