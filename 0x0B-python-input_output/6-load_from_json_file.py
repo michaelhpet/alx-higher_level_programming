185088aa7072228239adb6640549c43ae26257ab
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 21:46:29 +0100

    fix: json.load in place of json.loads

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -10,5 +10,5 @@ def load_from_json_file(filename):
         object: object loaded from file
     """
     import json
-    with open(filename, "w", encoding="utf-8") as file:
-        return json.loads(file.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
