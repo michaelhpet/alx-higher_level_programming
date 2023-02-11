b4f0eb7d3cd3a16f9f087af75d47137533fd7962
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 21:27:03 +0100

    feat: wrote from_json_string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Working with input/output in Python"""
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string
+    Args:
+        my_str (str): string to deserialize
+    Returns:
+        object: object represented by my_str
+    """
+    import json
+    return json.parse(my_str)
