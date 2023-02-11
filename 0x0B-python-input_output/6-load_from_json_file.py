05288f554ba062507dfba1f45800bf71f5d1ff8b
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 21:41:11 +0100

    feat: wrote load_from_json_file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Working with input/output in Python"""
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file
+    Args:
+        filename (str): name of file to load from
+    Returns:
+        object: object loaded from file
+    """
+    import json
+    with open(filename, "w", encoding="utf-8") as file:
+        return json.loads(file.read())
