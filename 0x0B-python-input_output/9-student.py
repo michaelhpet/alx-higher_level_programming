60bb70f1cbdb7cff738a34d01d05e95401576407
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 22:16:36 +0100

    feat: wrote class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Working with input/output in Python"""
+
+
+class Student:
+    """Defines a student
+    Attributes:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of student
+    """
+    def __init__(self, first_name. last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance
+        """
+        return self.__dict__
