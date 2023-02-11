4333457069608793fd419923ca7197a2d504613c
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 21:01:46 +0100

    fix: added encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
     Args:
         filename (str): name of file to read
     """
-    with open(filename) as file:
+    with open(filename, encoding="utf-8") as file:
         for line in file:
             print(line)
