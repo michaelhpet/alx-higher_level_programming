398d36aabbb7e9943904712f2ca5b509844252f8
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-11 21:13:27 +0100

    feat: wrote append_write

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Working with input/output in Python"""
+
+
+def write_file(filename="", text=""):
+    """Appends a string at the end of a text file (utf-8)
+    Args:
+        filename (str): name of file to write to
+        text (str): string to write into file
+    Returns:
+        int: total number of characters written
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
