f28615900665232dcad6d62240ab40a48d90f14b
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-12 08:50:13 +0100

    feat: wrote append_after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Working with input/output in Python"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file after each line
+        containing a specific string
+    Args:
+        filename (str): name of file to parse
+        search_string (str): string to append after
+        new_string (str): string to append
+    """
+    with open(filename, "r+", encoding="utf-8") as file:
+        to_write = ""
+        for line in file:
+            to_write += line
+            if search_string in line:
+                to_write += new_string
+        file.write(to_write)
