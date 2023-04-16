130e763d79314e5f21863f1fdcbd11fcd9e751ff
Michael Peter <michaelhpet@gmail.com>
2023-04-16 20:21:45 +0100

    test: write tests for to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class module."""
+import json
 
 
 class Base:
@@ -23,3 +24,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Get JSON representation of a list of dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,9 @@
 """Test module for Base class."""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import json
 
 
 class TestBase(unittest.TestCase):
@@ -24,6 +27,16 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base1.id, 12)
         self.assertEqual(base2.id, 54)
         self.assertNotEqual(base1.id, base2.id)
+
+    def test_to_json_string(self):
+        """Test to_json_string static method."""
+        rectangle_dict = Rectangle(42, 21, 3, 3, 77).to_dictionary()
+        square_dict = Square(13, 1, 4, 78).to_dictionary()
+        rectangle_json = Base.to_json_string([rectangle_dict])
+        square_json = Base.to_json_string([square_dict])
+
+        self.assertDictEqual(json.loads(rectangle_json)[0], rectangle_dict)
+        self.assertDictEqual(json.loads(square_json)[0], square_dict)
 
 
 if __name__ == "__main__":
