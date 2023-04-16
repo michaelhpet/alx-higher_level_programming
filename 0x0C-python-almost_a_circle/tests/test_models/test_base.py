21f823c895ef0f656a364acd016a1c3354edea5a
Michael Peter <michaelhpet@gmail.com>
2023-04-16 21:15:03 +0100

    test: add test cases for save_to_file and from_json_string

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,7 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 import json
+import os
 
 
 class TestBase(unittest.TestCase):
@@ -37,6 +38,33 @@ class TestBase(unittest.TestCase):
 
         self.assertDictEqual(json.loads(rectangle_json)[0], rectangle_dict)
         self.assertDictEqual(json.loads(square_json)[0], square_dict)
+
+    def test_from_json_string(self):
+        """Test from_json_string static method."""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+
+        self.assertListEqual(list_output,
+                             [{'height': 4, 'width': 10, 'id': 89},
+                              {'height': 7, 'width': 1, 'id': 7}])
+
+    def test_save_to_file(self):
+        """Test save_to_file method."""
+        filename = "Rectangle.json"
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open(filename, "r") as file:
+            self.assertListEqual(json.loads(file.read()), [
+                {"y": 8, "x": 2, "id": 1, "width": 10, "height": 7},
+                {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}
+            ])
+        os.remove(filename)
 
 
 if __name__ == "__main__":
