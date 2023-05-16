1f095f5a400f8cce4e760ec46fd639ca29adedb6
Michael Peter <michaelhpet@gmail.com>
2023-05-16 16:33:53 +0100

    feat: select columns and order by

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- list all records of the table second_table
+-- of the database hbtn_0c_0
+-- results should display both the score and the name (in this order)
+-- records should be ordered by score (top first)
+-- the database name will be passed as an argument
+SELECT score, name FROM second_table
+ORDER BY score DESC;
