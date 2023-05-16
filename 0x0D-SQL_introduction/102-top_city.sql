e28148d04df32257a49725b075f890b1e744335a
Michael Peter <michaelhpet@gmail.com>
2023-05-16 17:43:51 +0100

    fix: group by city

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- display the top 3 cities temperature during july and august ordered by temperature (desc)
 SELECT city, AVG(value) AS avg_temp FROM temperatures
 WHERE month IN (7, 8)
-GROUP BY value
+GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
