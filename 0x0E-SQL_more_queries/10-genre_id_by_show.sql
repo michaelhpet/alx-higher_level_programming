8dfa34179f94d41c8de95e3d5022ae9ec091b60f
Michael Peter <michaelhpet@gmail.com>
2023-05-17 18:35:22 +0100

    fix: remove database prefix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- each record should display tv_shows.title - tv_show_genres.genre_id
 -- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT t.`title`, g.`genre_id`
-FROM `hbtn_0d_tvshows`.`tv_shows` t
-INNER JOIN `hbtn_0d_tvshows`.`tv_show_genres` g
+FROM `tv_shows` t
+INNER JOIN `tv_show_genres` g
 ON t.`id` = g.`show_id`
 ORDER BY t.`title`, g.`genre_id`;
