7ecb137a9ba329e44effe192cbd3610853c8c696
Michael Peter <michaelhpet@gmail.com>
2023-05-17 18:01:21 +0100

    feat: more joins on tv shows and genres

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- list all shows contained in hbtn_0d_tvshows that have atlease one genre linked
+-- each record should display tv_shows.title - tv_show_genres.genre_id
+-- results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT t.title, g.genre_id
+FROM hbtn_0d_tvshows.tv_shows t
+JOIN hbtn_0d_tvshows.tv_show_genres g
+ON t.id = g.show_id
+ORDER BY t.title, g.genre_id;
