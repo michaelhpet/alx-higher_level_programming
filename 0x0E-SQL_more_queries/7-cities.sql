857b1b115727716236b8f72a580398216b0881a2
Michael Peter <michaelhpet@gmail.com>
2023-05-17 17:22:10 +0100

    fix: auto increment primary key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,10 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `cities` (
-	id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL
-		FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
