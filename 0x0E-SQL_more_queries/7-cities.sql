90211c31f829e5db64db3fb7d1c8b3e8ac0fa8cc
Michael Peter <michaelhpet@gmail.com>
2023-05-17 17:16:57 +0100

    fix: specify database name in foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,7 @@ USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `cities` (
 	id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
+	state_id INT NOT NULL
+		FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
