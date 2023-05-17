36d082622481ca10de9a6bf084aae19a41c355fe
Michael Peter <michaelhpet@gmail.com>
2023-05-17 16:24:45 +0100

    fix: show grants for users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list all privileges of users user_0d_1 and user_0d_2 in localhost
-SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
