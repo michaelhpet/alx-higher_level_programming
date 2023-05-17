97a94c496e27197d679ec5f0da148b9966b1c90a
Michael Peter <michaelhpet@gmail.com>
2023-05-17 16:43:15 +0100

    feat: create user, database, grant select permission

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- create the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+-- the user_0d_2 password should be set to user_0d_2_pwd
+-- if the datavase hbtn_0d_2 already exists, script should not fail
+-- if the user_0d_2 already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+
+GRANT SELECT PRIVILEGE ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
