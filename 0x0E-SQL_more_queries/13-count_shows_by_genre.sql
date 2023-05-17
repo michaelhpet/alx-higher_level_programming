04abc47484e33e505adff5abaee78ad28c45cd28
Michael Peter <michaelhpet@gmail.com>
2023-05-17 23:19:03 +0100

    fix: replace with *

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@
 -- second column must be called number_of_shows
 -- don't display a genre that doesn't have any shows linked
 -- results must be sorted in descending order by the number of shows linked
-SELECT g.`name` AS `genre`, COUNT(gs.`show_id`) as `number_of_shows`
+SELECT g.`name` AS `genre`, COUNT(*) as `number_of_shows`
 FROM `tv_genres` g
 INNER JOIN `tv_show_genres` gs
 ON g.`id` = gs.`genre_id`
