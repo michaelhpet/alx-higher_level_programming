2a63d272e9d41e42a027dd2a89690557276da001
Michael Peter <michaelhpet@gmail.com>
2023-05-18 00:22:24 +0100

    fix: select distinct

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,7 +2,7 @@
 -- the tv_genres table contains only one record where name = Comedy
 -- each record should display tv_shows.title
 -- results should be sorted in ascending order by the show title
-SELECT t.title
+SELECT DISTINCT t.title
 FROM tv_shows t
 LEFT JOIN tv_show_genres gs
 ON t.id = gs.show_id
