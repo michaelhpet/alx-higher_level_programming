cb6ab999944b0bd995874f9976110542aaa37664
Michael Peter <michaelhpet@gmail.com>
2023-05-17 18:40:14 +0100

    feat: more joins

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- list all shows contained in the database hbtn_0d_tvshows
+-- each record should display tv_shows.title - tv_show_genres.genre_id
+-- results must be sorted in ascending order by tv_show.title and tv_show_genres.genre_id
+--  if a show doesn't have a genre, display null
+SELECT t.title, g.genre_id
+FROM tv_shows t
+JOIN tv_show_genres g
+ON t.id = g.show_id
+ORDER BY t.title, g.genre_id;
