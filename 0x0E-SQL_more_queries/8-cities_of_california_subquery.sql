d69b8f88b11f623c82ac50124d53ca1a3bf48c40
Michael Peter <michaelhpet@gmail.com>
2023-05-17 17:36:08 +0100

    refactor: clean up query text formatting

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,9 +2,11 @@
 -- the states table contains only one record where name = California
 -- but the di can be different
 -- results must be sorted in ascending order by cities.id
-SELECT * FROM cities c
+SELECT id, name
+FROM cities c
 WHERE c.state_id IN (
-	SELECT id FROM states s
+	SELECT id
+	FROM states s
 	WHERE s.name = 'California')
 ORDER BY c.id ASC;
 
