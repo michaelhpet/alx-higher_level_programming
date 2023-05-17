f2128fcc08e0c84aae0bae44d4d6b6bb98faec00
Michael Peter <michaelhpet@gmail.com>
2023-05-17 18:41:31 +0100

    fix: left join to show null fields

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,6 +4,6 @@
 --  if a show doesn't have a genre, display null
 SELECT t.title, g.genre_id
 FROM tv_shows t
-JOIN tv_show_genres g
+LEFT JOIN tv_show_genres g
 ON t.id = g.show_id
 ORDER BY t.title, g.genre_id;
