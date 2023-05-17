7a5a6a812ea0ca35293a1010d98ad67557d7b26c
Michael Peter <michaelhpet@gmail.com>
2023-05-17 23:09:54 +0100

    feat: select distinct w/ group by

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- list all genres from hbtn_0d_tvshows and display the number of shows linked to each
+-- each record should display <TV Show genre> - <Number of shows linked to this genre>
+-- first column must be called genre
+-- second column must be called number_of_shows
+-- don't display a genre that doesn't have any shows linked
+-- results must be sorted in descending order by the number of shows linked
+SELECT DISTINCT g.name, COUNT(gs.show_id) as number_of_shows
+FROM tv_genres g
+JOIN tv_show_genres gs
+ON g.id = gs.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
