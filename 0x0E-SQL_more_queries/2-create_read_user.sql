1da3f349698fae6ff63cdd02d6e4842090f99630
Michael Peter <michaelhpet@gmail.com>
2023-05-17 16:48:05 +0100

    fix: grant select privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -8,4 +8,4 @@ IDENTIFIED BY 'user_0d_2_pwd';
 
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 
-GRANT SELECT PRIVILEGE ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
