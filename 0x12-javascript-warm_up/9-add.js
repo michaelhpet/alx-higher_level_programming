300cd73f8228d6f778a3d60fedb73e67ade57db1
Michael Peter <michaelhpet@gmail.com>
2023-06-13 17:58:08 +0100

    fix: semistandard error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ const firstNumber = Number(process.argv[2]);
 const secondNumber = Number(process.argv[3]);
 console.log(add(firstNumber, secondNumber));
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
