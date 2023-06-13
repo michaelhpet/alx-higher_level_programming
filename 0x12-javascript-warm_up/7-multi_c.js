f9b7df3c41a2823277892aaac7f7934fea269c61
Michael Peter <michaelhpet@gmail.com>
2023-06-13 17:39:47 +0100

    fix: use const variable

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let times = process.argv[2];
+const times = process.argv[2];
 if (!times || !Number(times)) console.log('Missing number of occurrences');
 else {
   for (let i = 0; i < times; i++) console.log('C is fun');
