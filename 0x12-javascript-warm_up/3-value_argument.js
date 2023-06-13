4dfba6627257ab5d89c894ee18289e390eb4d115
Michael Peter <michaelhpet@gmail.com>
2023-06-13 17:19:00 +0100

    fix: remove array from

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const args = Array.from(process.argv).slice(2);
+const args = process.argv.slice(2);
 const message = typeof args[0] === 'undefined' ? 'No argument' : args[0];
 console.log(message);
