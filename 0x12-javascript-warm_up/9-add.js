4c55254cc948fa19c5cb7136b82c6734165f5743
Michael Peter <michaelhpet@gmail.com>
2023-06-13 17:57:00 +0100

    feat: program to add two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstNumber = Number(process.argv[2]);
+const secondNumber = Number(process.argv[3]);
+console.log(add(firstNumber, secondNumber));
+
+function add(a, b) {
+  return a + b;
+}
