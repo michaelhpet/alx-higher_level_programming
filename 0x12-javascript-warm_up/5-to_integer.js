be8d0a5ff3cc9b534a64fc38039994a80eeba038
Michael Peter <michaelhpet@gmail.com>
2023-06-13 17:28:30 +0100

    fix: use indexing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const args = Array.from(process.argv).slice(2);
-const number = Number(args?.at(0));
+const number = Number(args[0]);
 if (!number) console.log('Not a number');
 else console.log(`My number: ${number}`);
