a7b9d20a547041ddc8cef06e3f3b8a64137d5f1e
Michael Peter <michaelhpet@gmail.com>
2023-06-19 23:33:19 +0100

    feat: query first object

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""First state.
+
+A script to print the first `State` object from the database
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+def main():
+    """Entry point of program."""
+    engine = create_engine(
+        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).limit(1)
+    if states.count() == 0:
+        print("Nothing")
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    main()
