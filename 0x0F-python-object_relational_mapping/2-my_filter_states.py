538c6fb7203a07c14743926f5dbe34d474af3263
Michael Peter <michaelhpet@gmail.com>
2023-06-19 20:31:30 +0100

    feat: more queries with mysqldb py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Filter states by user input.
+
+A script to list all states where `name` matches the argument
+- takes 3 arguments: `mysql username`, `state name searched`
+, `mysql password`, `database name`
+- connects to MySQL server running on `localhost` at port `3306`
+- results are sorted in ascending order by states.id
+"""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Entry point of program."""
+    connection = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM states WHERE name = '{argv[4]}';")
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
