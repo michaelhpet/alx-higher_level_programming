567fff35db4e492fe1421dfd521ac600a8259f88
Michael Peter <michaelhpet@gmail.com>
2023-07-02 21:16:11 +0100

    feat: print response header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Send a request and display value of X-Request-Id."""
+from urllib.request import Request, urlopen
+from sys import argv
+
+
+def main():
+    """Entry point of program."""
+    url = argv[1]
+    request = Request(url)
+    with urlopen(request) as response:
+        headers = dict(response.headers)
+        print(headers["X-Request-Id"])
+
+
+if __name__ == "__main__":
+    main()
