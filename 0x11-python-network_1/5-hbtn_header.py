e53d531529aab18dfeff6b2576d860bd9bfa4124
Michael Peter <michaelhpet@gmail.com>
2023-07-02 21:44:46 +0100

    feat: display response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Send a request and display X-Request-Id header."""
+import requests
+from sys import argv
+
+
+def main():
+    """Entry point of program."""
+    url = argv[1]
+    response = requests(url)
+    print(response.headers.get("X-Request-Id"))
+
+
+if __name__ == "__main__":
+    main()
