74c15c71fbe4e25c67b927446b8aa5367346eaf1
Michael Peter <michaelhpet@gmail.com>
2023-07-02 22:24:12 +0100

    fix: full import sys

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Get 10 most recent commits in repository."""
 import requests
-from sys import argv
+import sys
 
 
 def main():
     """Entry point of program."""
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        argv[2],
-        argv[1]
+        sys.argv[2],
+        sys.argv[1]
     )
     response = requests.get(url)
     commits = response.json()
