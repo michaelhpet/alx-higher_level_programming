7ec8f25e086b76d75373c1b3fccc19e410c2a84d
Michael Peter <michaelhpet@gmail.com>
2023-07-02 22:22:17 +0100

    fix: use string format

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,16 +6,20 @@ from sys import argv
 
 def main():
     """Entry point of program."""
-    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[2],
+        argv[1]
+    )
     response = requests.get(url)
     commits = response.json()
     try:
-        for i in range(10):
+        commits = commits[:10]
+        for commit in commits:
             print("{} {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")
+                commit.get("sha"),
+                commit.get("commit").get("author").get("name")
             ))
-    except IndexError:
+    except Exception:
         pass
 
 
