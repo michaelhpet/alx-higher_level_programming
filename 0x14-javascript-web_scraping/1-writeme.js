631d5f32b5440892828d95870707f56ea1e35526
Michael Peter <michaelhpet@gmail.com>
2023-07-25 21:57:19 +0100

    feat: node write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require("fs");
+
+const filename = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(filename, content, { encoding: "utf-8" }, (err) => {
+  if (err) console.log(err);
+});
