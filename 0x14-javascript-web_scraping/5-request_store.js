1f04de5aa36296f0e46d70871570b3696df96862
Michael Peter <michaelhpet@gmail.com>
2023-07-25 23:44:16 +0100

    chore: cleanup

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require("fs");
+const request = require("request");
+
+const endpoint = process.argv[2];
+const filename = process.argv[3];
+request(endpoint).pipe(fs.createWriteStream(filename));
