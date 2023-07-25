a932621dd163e5407d8b55c81aa02146b75a0b8e
Michael Peter <michaelhpet@gmail.com>
2023-07-25 23:07:46 +0100

    fix: semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const filename = process.argv[2];
-fs.readFile(filename, { encoding: "utf-8" }, (err, data) => {
+fs.readFile(filename, { encoding: 'utf-8' }, (err, data) => {
   console.log(err ?? data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
 const filename = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filename, content, { encoding: "utf-8" }, (err) => {
+fs.writeFile(filename, content, { encoding: 'utf-8' }, (err) => {
   if (err) console.log(err);
 });
