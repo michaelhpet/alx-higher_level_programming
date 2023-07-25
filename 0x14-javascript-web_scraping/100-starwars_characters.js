efaada72770fa9a6e309d4e35890b5a2d4a7d9d8
Michael Peter <michaelhpet@gmail.com>
2023-07-26 00:50:51 +0100

    feat: characters in order

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2];
+const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+request(endpoint, { method: 'GET' }, (error, _, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters, 0);
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+function printCharacters (characters, i) {
+  request(characters[i], { method: 'GET' }, (error, _, body) => {
+    if (error) {
+      console.log(error);
+      return;
     }
+    console.log(JSON.parse(body).name);
+    if (i < characters.length - 1) printCharacters(characters, ++i);
   });
 }
