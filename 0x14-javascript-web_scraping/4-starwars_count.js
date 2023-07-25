c6c16801b9037a0873eecb45e6fd9e435a4e3595
Michael Peter <michaelhpet@gmail.com>
2023-07-25 23:53:31 +0100

    chore: cleanup

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,9 @@
 const request = require('request');
 
 const endpoint = process.argv[2];
-request(endpoint, function (error, _, body) {
+request(endpoint, (error, _, body) => {
   if (error) return;
+
   const results = JSON.parse(body).results;
   const count = results.reduce((count, m) => {
     return m.characters.find((c) => c.endsWith('/18/')) ? count++ : count;
