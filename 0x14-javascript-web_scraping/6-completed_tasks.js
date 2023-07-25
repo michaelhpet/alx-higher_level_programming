1b011b636db4e12d5edad03b41cc47d64dd67384
Michael Peter <michaelhpet@gmail.com>
2023-07-26 00:12:13 +0100

    feat: completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const endpoint = process.argv[2];
+
+request(endpoint, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode !== 200) {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  } else {
+    const tasks = JSON.parse(body);
+    const completed = tasks.reduce((completed, task) => {
+      if (task.completed && !completed[task.userId]) completed[task.userId] = 1;
+      else completed[task.userId]++;
+      return completed;
+    }, {});
+
+    console.log(completed);
+  }
+});
