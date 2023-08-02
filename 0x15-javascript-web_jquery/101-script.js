bac82991490f28b8bf374df7977d10650018e432
Michael Peter <michaelhpet@gmail.com>
2023-08-02 10:56:56 +0100

    fix: semistandard style fixes

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,19 +1,19 @@
 $(function () {
-  const myList = $("ul.my_list");
-  const addItem = $("div#add_item");
-  const removeItem = $("div#remove_item");
-  const clearList = $("div#clear_list");
+  const myList = $('ul.my_list');
+  const addItem = $('div#add_item');
+  const removeItem = $('div#remove_item');
+  const clearList = $('div#clear_list');
 
   addItem.click(() => {
-    myList.append("<li>Item</li>");
+    myList.append('<li>Item</li>');
   });
 
   removeItem.click(() => {
-    lastItem = $("ul.my_list li:last-child")[0];
+    lastItem = $('ul.my_list li:last-child')[0];
     if (lastItem) lastItem.remove();
   });
 
   clearList.click(() => {
-    myList.html("");
+    myList.html('');
   });
 });
